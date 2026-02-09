d726e8d6360185d885194302aab9b9014c231ce0
MojitoMonday <joespicola@gmail.com>
2026-02-09 16:06:54 -0500

    remove unneeded scripts

--- launch.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-# Bash script that runs the gx2bot
-# Used for remote shenanigans
-pipenv shell 'nohup python main.py --gx2bot &'
--- update.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-# Bash script that updates the gx2bot code
-# Used for remote shenanigans
-git reset --hard
-git pull
-pipenv shell "pipenv sync"
