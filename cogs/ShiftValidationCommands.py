61dc2320da833f96292ca489d01b07b998d8c219
Mojito <joespicola@gmail.com>
2026-02-11 16:33:34 -0500

    cleanup shift validation

--- cogs/ShiftValidationCommands.py
+++ cogs/ShiftValidationCommands.py
@@ -1,7 +1,13 @@
+from config import (
+    SHEET_DISCORD_MEMBERS,
+    SHEET_DISCORD_MEMBERS_ID_COL,
+    SHEET_DISCORD_MEMBERS_NAME_COL,
+    SHEET_DISCORD_MEMBERS_NICKNAME_COL,
+    SHEET_DISCORD_MEMBERS_VOLUNTEERS_TAB,
+    SHIFT_VALIDATION_WEBHOOK_ID,
+)
 from discord.ext import commands
-from discord.utils import get
 import logging  # to log obviously
-import os  # to get local files like credentials and such
 from pprint import pprint  # to pretty print lists and dictionaries
 from functools import lru_cache
 
@@ -25,43 +31,44 @@ class ShiftValidationCommands(commands.Cog):
     @commands.Cog.listener("on_message")
     async def notify_rewards_from_webhook(self, msg):
         # TODO: Change hardcoded webhook id
-        if msg.webhook_id == 1337111947551703142:
-            # parse the msg
-            content = msg.content
-            command = content.split()[0]
-            if command != "!notify_rewards":
-                return  # something else, not handling here
+        if msg.webhook_id != SHIFT_VALIDATION_WEBHOOK_ID:
+            return
 
-            user_name = content.split()[1]
-            message = " ".join(content.split()[2:])
+        content = msg.content
+        command = content.split()[0]
+        if command != "!notify_rewards":
+            return  # something else, not handling here
 
-            volunteer_data = get_values(
-                spreadsheet=os.getenv("SHEET_DISCORD_MEMBERS"), sheet="SYF"
-            )
-            # print(volunteer_data)
-            user_data = None
-            # Currently hardcoded, would want to update to be dynamic
-            id_col = 0
-            name_col = 1
-            nick_col = 3
-            # TODO Change to get_requester_row
-            for user_details in volunteer_data[
-                1:
-            ]:  # ignore the header line, cause im not dealing with pandas at the moment
-                if user_name.lower() in [
-                    user_details[name_col].lower(),
-                    user_details[nick_col].lower(),
-                ]:
-                    user_data = user_details
-                    break
-            if user_data is None:
-                print(f"User {user_name} not found")
-                await msg.delete()
-                return
+        username = content.split()[1]
+        message = " ".join(content.split()[2:])
 
-            user = self.bot.get_user(int(user_data[id_col]))
-            await user.send(message)
+        volunteer_data = get_values(
+            spreadsheet=SHEET_DISCORD_MEMBERS,
+            sheet=SHEET_DISCORD_MEMBERS_VOLUNTEERS_TAB,
+        )
+
+        user_data = get_user_data(username, volunteer_data)
+
+        if user_data is None:
+            print(f"User {username} not found")
             await msg.delete()
+            return
+
+        user = self.bot.get_user(int(user_data[SHEET_DISCORD_MEMBERS_ID_COL]))
+        await user.send(message)
+        await msg.delete()
+
+
+def get_user_data(username, volunteer_data: list) -> dict:
+    # TODO Change to get_requester_row
+    # ignore the header line, cause im not dealing with pandas at the moment
+    for user_details in volunteer_data[1:]:
+        if username.lower() in [
+            user_details[SHEET_DISCORD_MEMBERS_NAME_COL].lower(),
+            user_details[SHEET_DISCORD_MEMBERS_NICKNAME_COL].lower(),
+        ]:
+            return user_details
+    return None
 
 
 async def setup(bot):
